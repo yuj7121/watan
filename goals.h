47be209110bb7fe11d534aa5f5ca0eb0458493d7
angela jin <155695803+angelajinn@users.noreply.github.com>
2024-11-22 15:50:00 -0500

    goals.cc implements goals.h

---
+++ goals.h
@@ -0,0 +1,17 @@
+#ifndef GOALS_H
+#define GOALS_H
+
+#include <iostream> 
+#include <vector> 
+#include <string>
+using namespace std; 
+
+class Goals {
+    int index; 
+    Student* owner; 
+    vector<int> Adjacent;
+};
+
+
+
+#endif
