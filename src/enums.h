4618788262708460b828e891af9f7b5009a00d6f
yujin <y2bae@uwaterloo.ca>
2024-11-22 15:36:34 -0500

    a

---
+++ src/enums.h
@@ -0,0 +1,8 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+// ENUMS.H: this header file includes all enums we will uses
+
+enum class ResourceType { CAFFEINE, LAB, LECTURE, STUDY, TUTORIAL, NETFLIX };
+enum class Colour { BLUE, RED, ORANGE, YELLOW };
+
+#nedif
