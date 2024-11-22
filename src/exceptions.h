c7aa5e60b6e511fc46d8ebf47e2b3c14c5709f9e
halle <hallechanne@gmail.com>
2024-11-21 23:34:31 -0500

    exceptions.h is raised from dirt.

---
+++ src/exceptions.h
@@ -0,0 +1,69 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+// EXCEPTIONS.H: A series of in-game exceptions as defined by three categories:
+// (a) File I/O Errors, (b) Invalid User Input, and (c) Insufficient Conditions.
+
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+// (a) File I/O Errors
+class FileNotFoundException : public runtime_error {
+    public:
+        explicit FileNotFoundException(const string& note);
+};
+
+class FileFormatException : public runtime_error {
+    public:
+        explicit FileFormatException(const string& note);
+};
+
+class FileWriteException : public runtime_error {
+    public:
+        explicit FileWriteException(const string& note);
+};
+
+// (b) Invalid User Input
+class InvalidInputException : public invalid_argument {
+    public:
+        explicit InvalidInputException(const string& note);
+};
+
+class OutOfRangeInputException : public out_of_range {
+    public:
+        explicit OutOfRangeInputException(const string& note);
+};
+
+// (c) Insufficient Conditions
+class CriterionAlreadyPurchasedException : public logic_error {
+public:
+    explicit CriterionAlreadyPurchasedException(const string &note);
+};
+
+class NonAdjacentPlacementException : public logic_error {
+public:
+    explicit NonAdjacentPlacementException(const string &note);
+};
+
+class InsufficientResourcesException : public logic_error {
+public:
+    explicit InsufficientResourcesException(const string &note);
+};
+
+class GoalAlreadyAchievedException : public logic_error {
+public:
+    explicit GoalAlreadyAchievedException(const string &note);
+};
+
+class InvalidCriterionImprovementException : public logic_error {
+public:
+    explicit InvalidCriterionImprovementException(const string &note);
+};
+
+class InvalidGeesePlacementException : public logic_error {
+public:
+    explicit InvalidGeesePlacementException(const string &note);
+};
+
+#endif
