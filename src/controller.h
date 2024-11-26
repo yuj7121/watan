89d58a6400b1c448c59547ce37918890d0773feb
angela jin <155695803+angelajinn@users.noreply.github.com>
2024-11-25 20:51:48 -0500

    implement roll()

--- src/controller.h
+++ src/controller.h
@@ -13,9 +13,19 @@ using namespace std;
 
 class Controller {
     const int winVictoryPoints = 10;
+    std::unique_ptr<Board> theBoard;
+    std::unique_ptr<Dice> dice;
+    std::default_random_engine *eng;
 public: 
     Controller(); 
-    
+
+    void newGame();
+    void loadGame(const std::string file);
+    void loadBoard(const std::string file);
+
+    // val = loaded dice value (type == false)
+    // type is true if fair, and false if loaded
+    void rollDice(int val = -1, bool type); 
 }; 
 
 #endif
