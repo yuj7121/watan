c65aec7ff86cc24a82e4a04a05e790c9e13600f2
angela jin <155695803+angelajinn@users.noreply.github.com>
2024-11-26 14:44:12 -0500

    newGame()

--- src/gameplay.cc
+++ src/gameplay.cc
@@ -1,19 +1,41 @@
 #include "gameplay.h"
 
-
 using namespace std; 
 
 Gameplay::Gameplay() {}
 
 void Gameplay::newGame() {
-    array<pair<int, int>, 19> tiles;
-	array<int, 6> availableResources{ {3, 3, 4, 4, 4, 1} };
-	array<int, 12> availableValues{ {0, 1, 2, 2, 2, 2, 0, 2, 2, 2, 2, 1} };
+    vector<pair<int, int>> tiles(19); 
+	vector<int> availableResources = {3, 3, 4, 4, 4, 1};
+	vector<int> availableValues = {0, 1, 2, 2, 2, 2, 0, 2, 2, 2, 2, 1};
 
     for (int i = 0; i < 19; ++i) {
-        
-    }
 
+        uniform_int_distribution<int> rDistribution(0, 5);
+        int randomResource = rDistribution(eng); 
+
+        while (availableResources.at(randomResource) == 0) {
+            randomResource = rDistribution(eng); 
+        }
+        --availableResources.at(randomResource); 
+
+        int randomValue = 0; 
+        if (randomResource = 5) {
+            randomValue = 7; // NETFLIX resource 
+        } else { 
+            uniform_int_distribution<int> vDistribution(1, 12); 
+            randomValue = vDistribution(eng); 
+
+            while (availableValues.at(randomValue - 1) == 0) {
+                randomValue = rDistribution(eng); 
+            }
+            --availableValues.at(randomValue - 1); 
+        }
+
+        pair<int, int> thisTile{randomResource, randomValue); 
+        tiles.at(i) = thisTile; 
+    }
+    // TODO: transfer this information to the board/text display
 }
 
 void Gameplay::rollDice(int val = -1, bool isFairDice = false) {
