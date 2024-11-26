42e2790e777c5916991de7a3c3bb614b4842d0bc
angela jin <155695803+angelajinn@users.noreply.github.com>
2024-11-25 21:52:01 -0500

    a

--- src/controller.h
+++ src/controller.h
@@ -8,16 +8,21 @@
 #include "dice.h"
 #include "boardsetup.h"
 #include "constants.h"
+#include "student.h"
 
 using namespace std; 
 
-class Controller {
+class Gameplay {
     const int winVictoryPoints = 10;
     std::unique_ptr<Board> theBoard;
+    std::vector<std::unique_ptr<Student>> Students;
     std::unique_ptr<Dice> dice;
     std::default_random_engine *eng;
+
+    int whoseTurn; 
+
 public: 
-    Controller(); 
+    Gameplay(); 
 
     void newGame();
     void loadGame(const std::string file);
@@ -26,6 +31,10 @@ public:
     // val = loaded dice value (type == false)
     // type is true if fair, and false if loaded
     void rollDice(int val = -1, bool type); 
+
+    string getWhoseTurn() const; 
+    bool gameOver() const; 
+
 }; 
 
 #endif
