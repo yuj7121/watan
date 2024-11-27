40b2ca1dbca5f3102ba4807e44cf72963943f0e6
angela jin <155695803+angelajinn@users.noreply.github.com>
2024-11-26 20:44:43 -0500

    newBoard()

--- src/gameplay.cc
+++ src/gameplay.cc
@@ -6,14 +6,70 @@ using namespace std;
 Gameplay::Gameplay() {}
 
 void Gameplay::newGame() {
-    array<pair<int, int>, 19> tiles;
-	array<int, 6> availableResources{ {3, 3, 4, 4, 4, 1} };
-	array<int, 12> availableValues{ {0, 1, 2, 2, 2, 2, 0, 2, 2, 2, 2, 1} };
+    setup = std::make_unique<RandomSetup>();
+    setup->setup(); 
+    // TODO: text display and board display
+}
 
-    for (int i = 0; i < 19; ++i) {
-        
+void Gameplay::loadGame(const std::string file) { 
+    ifstream f{file};
+	int curTurn;
+    // TODO: check types 
+    vector<vector<int>> studentResources(4, vector<int>(5));
+    vector<vector<int>> studentGoals(4);
+    vector<vector<pair<int, int>>> studentCriteria(4);
+	int geese;
+    for (int i = 0; i < 7; ++i) {
+        string line; 
+        getline(f, line); 
+        istringstream iss{line}; 
+        int input; 
+        while (iss >> input) {
+            // curTurn
+            if (i == 0) {
+                curTurn = input; 
+            // studentData
+            } else if (i >= 1 && i <= 4) {
+                int playerIndex = i - 4; 
+                vector<int> resources(5); 
+                for (int j = 0; j < 5; ++j) {
+                    resources.at(j) = input; 
+                    iss >> input; 
+                }
+                studentResources.at(playerIndex) = resources; 
+                if (input == static_cast<int>('g')) iss >> input; 
+                // goals
+                vector<int> goals; 
+                while (input != static_cast<int>('c')) {
+                    goals.emplace_back(input); 
+                    iss >> input; 
+                }
+                studentGoals.at(playerIndex) = goals; 
+                if (input == static_cast<int>('c')) iss >> input; 
+                // criteria
+                vector<pair<int, int>> criteria; 
+                while (iss >> input) {
+                    int level; 
+                    iss >> level; 
+                    criteria.emplace_back(input, level); 
+                }
+                studentCriteria.at(playerIndex) = criteria; 
+            // board 
+            } else if (i == 5) {
+                setup = std::make_unique<FileSetup>(file); 
+                setup->setup(); 
+            } else {
+                geese = input; 
+            }
+        }
     }
 
+    // TODO: text display and board display
+}
+void Gameplay::loadBoard(const std::string file) {
+    setup = std::make_unique<FileSetup>(file); 
+    setup->setup(); 
+    // TODO: text display and board display 
 }
 
 void Gameplay::rollDice(int val = -1, bool type) {
